9a20101c721992f260572833e586d6f72626be82
ruth1104 <ruthdu30@gmail.com>
2024-09-03 10:43:24 -0500

    Ruta enfermedad
    Arreglo de ruta enfermedad

--- app/models/enfermedades.py
+++ app/models/enfermedades.py
@@ -5,6 +5,6 @@ class Enfermedades(db.Model):
     __tablename__ = 'enfermedad'
     idEnfermedad = db.Column(db.Integer, primary_key=True)
     nombreEnfermedad = db.Column(db.String(255), nullable=False)
-    sintomas= db.Column(db.String(255), nullable=False)
-    detallesAdicionales = db.Column(db.String(255), nullable=False)
+    sintomasEnfermedad= db.Column(db.String(255), nullable=False)
+    detallesAdicionalesEnfermedad = db.Column(db.String(255), nullable=False)
     
