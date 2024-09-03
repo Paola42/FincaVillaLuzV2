b72a59bf4b5d9a4b7a02d9b4ce54f93b23ee3010
ruth1104 <ruthdu30@gmail.com>
2024-09-03 11:24:33 -0500

    Ruta forraje

--- app/models/forrajes.py
+++ app/models/forrajes.py
@@ -4,10 +4,10 @@ from app import db
 class Forrajes(db.Model):
     __tablename__ = 'forraje'
     idForraje = db.Column(db.Integer, primary_key=True)
-    especie= db.Column(db.String(255), nullable=False)
-    fechaDeSiembra =db.Column(db.db.Date,nullable=True)
-    fechaDeCosecha =db.Column(db.db.Date,nullable=True)
-    area = db.Column(db.String(255), nullable=False)
-    manejos  = db.Column(db.String(255), nullable=False)
-    aforo  =db.column(db.String(255),nullable=True)
+    especieForraje= db.Column(db.String(255), nullable=False)
+    fechaDeSiembraForraje =db.Column(db.db.Date,nullable=True)
+    fechaDeCosechaForraje =db.Column(db.db.Date,nullable=True)
+    areaForraje = db.Column(db.String(255), nullable=False)
+    manejosForraje  = db.Column(db.String(255), nullable=False)
+    aforoForraje  =db.column(db.String(255),nullable=True)
     forraje  = db.Column(db.Integer, db.ForeignKey('forraje.idForraje'))
