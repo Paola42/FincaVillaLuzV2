63cd1b852390b8615d2c45e884f59f12c9c57094
Paola42 <116293643+Paola42@users.noreply.github.com>
2024-09-24 09:05:35 -0500

    Update animalesMejorados.py

--- app/models/animalesMejorados.py
+++ app/models/animalesMejorados.py
@@ -7,7 +7,7 @@ class AnimalesMejorados(db.Model):
     idPadreAportante = db.column(db.Integer,nullable=False)
     nombrePadreAportante = db.Column(db.String(255), nullable=False)
     razaPadreAportante = db.Column(db.String(255), nullable=False)
-    idMadreAportante = db.column(db.Integer,nullable=False)
+    idMadreAportante = db.Column(db.Integer,nullable=False)
     nombreMadreAportante = db.Column(db.String(255), nullable=False)
     razaMadreAportante = db.Column(db.String(255), nullable=False)
     animal = db.Column(db.Integer, db.ForeignKey('animal.idAnimal'))
